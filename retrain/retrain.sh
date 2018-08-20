c0ae231f713fd4390c8003ff73a32c44595ba3ec
bei1994 <31449017+bei1994@users.noreply.github.com>
2018-08-19 23:35:53 -0700

    Delete retrain.sh

--- retrain/retrain.sh
+++
@@ -1,7 +0,0 @@
-python3 /Users/lorenzostudyroom16/Desktop/tensorflow\ exercise/tensorflow/retrain.py \
---bottleneck_dir /Users/lorenzostudyroom/Desktop/tensorflow\ exercise/retrain/bottleneck \
---how_many_training_steps 200 \
---model_dir /Users/lorenzostudyroom/Desktop/tensorflow\ exercise/inception_model/inception_v3_2015_12_05/ \
---output_graph output_graph.pb \
---output_labels output_labels.txt \
---image_dir /Users/lorenzostudyroom/Desktop/tensorflow\ exercise/retrain/data/train/
